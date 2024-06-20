627a88a195cd34efe9c76f73d0a00d242934b1d8
Leyon <leyonik995@gmail.com>
2024-06-20 15:49:16 -0400

    Created models for Trainer and Client

--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Trainer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()  #email
+    contact = models.CharField(max_length=20)  #phone numbers
+    website = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact = models.CharField(max_length=20)
+    age = models.PositiveIntegerField(default=0)
+
+    MALE = 'Male'
+    FEMALE = 'Female'
+    OTHER = 'Other'
+
+    GENDER_CHOICES = {
+        MALE: 'Male',
+        FEMALE: 'Female',
+        OTHER: 'Other',
+    }
+
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default=OTHER)
+
+    def __str__(self):
+        return self.name
