233d6c9568cb67b0f73adc747f45e23bfcdf3251
Leyon <leyonik995@gmail.com>
2024-06-27 13:21:24 -0400

    Added GymUser, Trainer and Client models to admin page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import GymUser, Trainer, Client
+
 # Register your models here.
+
+admin.site.register(GymUser)
+admin.site.register(Trainer)
+admin.site.register(Client)
