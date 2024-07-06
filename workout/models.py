9581d705565afe765f8abcf0e742b19e8b0ba99e
Leyon <leyonik995@gmail.com>
2024-07-06 15:54:52 -0400

    Update workout model to reflect who it is for and who created it (client & trainer)

--- workout/models.py
+++ workout/models.py
@@ -1,11 +1,16 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
 
+from user.models import Trainer, Client
+
 
 class Workout(models.Model):
     name = models.CharField(max_length=100, verbose_name="Workout Name")
     description = models.TextField(verbose_name="Workout Description", help_text="Detailed description of the workout.")
     days_per_week = models.PositiveIntegerField(validators=[MaxValueValidator(7)], verbose_name="Days per Week")
+
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, verbose_name="Trainer")
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Client")
 
     def __str__(self):
         return self.name
