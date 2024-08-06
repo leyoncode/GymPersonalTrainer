db41df3594f844d3ee26103328a9884d17bee6c2
Leyon <leyonik995@gmail.com>
2024-08-06 15:38:11 -0400

    Changed client registration form to enter their trainer's username which the backend automatically connects to the trainer in the database as foriegnkey
    This was done as there could potentially be millions of trainers and we don't want to make users search and find their trainer in the middle of the haystack. Plus it is optimal for the frontend and network bandwidth to have less unnecessary data being sent from the server to the browsers.

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import GymUser, Trainer, Client
+from django.core.exceptions import ValidationError
 
 
 class GymUserRegistrationForm(UserCreationForm):
@@ -23,7 +24,34 @@ class TrainerRegistrationForm(forms.ModelForm):
         fields = ['trainer_contact', 'website']
 
 
+# class ClientRegistrationForm(forms.ModelForm):
+#     class Meta:
+#         model = Client
+#         fields = ['user_contact', 'age', 'weight_in_lbs', 'reason_for_training', 'gender', 'trainer']
+#
 class ClientRegistrationForm(forms.ModelForm):
+    trainer_username = forms.CharField(required=True, help_text="Enter your trainer's username.")
+
     class Meta:
         model = Client
-        fields = ['user_contact', 'age', 'weight_in_lbs', 'reason_for_training', 'gender', 'trainer']
+        fields = ['user_contact', 'age', 'weight_in_lbs', 'reason_for_training', 'gender', 'trainer_username']
+
+    def clean_trainer_username(self):
+        username = self.cleaned_data.get('trainer_username')
+        try:
+            trainer = Trainer.objects.get(user__username=username)
+            print("No error detected")
+            return trainer
+        except Trainer.DoesNotExist:
+            print("Errror detected")  # DEBUG
+            raise forms.ValidationError("A trainer with this username does not exist.")
+
+    def save(self, commit=True):
+        client = super(ClientRegistrationForm, self).save(commit=False)
+        # Set the trainer to the Trainer object fetched from the cleaned data
+        client.trainer = self.cleaned_data.get('trainer_username')
+
+        if commit:
+            client.save()
+
+        return client
